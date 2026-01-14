f957e4a2c45cecdaa4ee6e9d8b3c032592373a65
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-01-14 16:29:04 +0100

    asyncread

--- src/gateway/quic/quic_actor/stream.rs
+++ src/gateway/quic/quic_actor/stream.rs
@@ -1,10 +1,10 @@
 use crate::gateway::quic::utils::BufPool;
 use bytes::Bytes;
 use futures::task::AtomicWaker;
-use quinn_proto::{Connection, Event, StreamEvent, StreamId, WriteError};
+use quinn_proto::{Connection, Event, ReadError, ReadableError, StreamEvent, StreamId, WriteError};
 use rtrb::{Consumer, Producer};
 use std::cmp::min;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, HashSet};
 use std::io::IoSlice;
 use std::pin::Pin;
 use std::sync::atomic::{fence, AtomicBool, Ordering};
@@ -65,17 +65,14 @@ impl AsyncRead for RecvStream {
         let mut chunks = this.recv.read_chunk(slots).unwrap().into_iter();
         loop {
             if let Some(pending) = &mut this.pending {
-                if pending.is_empty() {
-                    this.closed.store(true, Ordering::Relaxed);
-                    return Poll::Ready(Ok(()));
+                if !pending.is_empty() {
+                    let len = min(buf.remaining(), pending.len());
+                    buf.put_slice(&pending.split_to(len));
                 }
-                let len = min(buf.remaining(), pending.len());
-                buf.put_slice(&pending.split_to(len));
-                if pending.is_empty() {
-                    this.pending = None;
-                } else {
+                if !pending.is_empty() {
                     break;
                 }
+                this.pending = None;
             }
 
             debug_assert!(this.pending.is_none());
@@ -130,14 +127,14 @@ impl AsyncWrite for SendStream {
         if self.blocked.swap(false, Ordering::SeqCst)
             && let Err(_) = self.evt_tx.send(StreamEvt::SendUnblocked(self.id))
         {
-            self.closed.store(true, Ordering::Relaxed);
+            self.closed.store(true, Ordering::Release);
         }
         Poll::Ready(Ok(()))
     }
 
     fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
         let _ = self.evt_tx.send(StreamEvt::SendClosed(self.id));
-        self.closed.store(true, Ordering::Relaxed);
+        self.closed.store(true, Ordering::Release);
         Poll::Ready(Ok(()))
     }
 
@@ -174,8 +171,9 @@ impl AsyncWrite for SendStream {
         // once `blocked` is true, downstream cannot change it back to false
         // the next poll_write (or poll_flush) wakes downstream if the value of `blocked` is lagged
         // I suppose poll_flush will always be called
-        if slots - n <= QUIC_STREAM_SEND_UNBLOCK_THRESHOLD
-            && this.blocked.swap(false, Ordering::Relaxed)
+        if ((n == QUIC_STREAM_SEND_BUF_CAP && this.blocked.swap(false, Ordering::SeqCst))
+            || (slots - n <= QUIC_STREAM_SEND_UNBLOCK_THRESHOLD
+            && this.blocked.swap(false, Ordering::Relaxed)))
             && let Err(_) = this.evt_tx.send(StreamEvt::SendUnblocked(this.id))
         {
             this.closed.store(true, Ordering::Release);
@@ -187,6 +185,13 @@ impl AsyncWrite for SendStream {
     fn is_write_vectored(&self) -> bool {
         true
     }
+}
+
+struct RecvStreamCtx {
+    recv: Producer<Bytes>,
+    waker: Arc<AtomicWaker>,
+    blocked: Arc<AtomicBool>,
+    closed: Arc<AtomicBool>,
 }
 
 struct SendStreamCtx {
@@ -203,8 +208,11 @@ struct Runner {
 impl Runner {
     fn run(&mut self, mut conn: Connection) {
         let mut writable_streams = HashSet::new();
-        let mut blocked_streams = HashSet::new();
+        let mut readable_streams = HashSet::new();
         let mut send_streams = HashMap::<StreamId, SendStreamCtx>::new();
+        let mut recv_streams = HashMap::<StreamId, RecvStreamCtx>::new();
+        let mut blocked_send_streams = HashSet::new();
+        let mut blocked_recv_streams = HashSet::new();
         // event loop
         loop {
             while let Some(evt) = conn.poll() {
@@ -212,18 +220,97 @@ impl Runner {
                     Event::Stream(StreamEvent::Writable { id }) => {
                         writable_streams.insert(id);
                     }
+                    Event::Stream(StreamEvent::Readable { id }) => {
+                        readable_streams.insert(id);
+                    }
                     _ => todo!(),
                 }
             }
-            
+
             while let Ok(evt) = self.evt_rx.try_recv() {
                 match evt {
                     StreamEvt::SendUnblocked(id) => {
-                        blocked_streams.remove(&id);
+                        blocked_send_streams.remove(&id);
                     }
-                    _ => todo!()
+                    StreamEvt::SendClosed(id) => {
+                        // if this stream is still open in quinn, wait for the next Writable event (or closed event)
+                        // else delete it
+                    }
+                    StreamEvt::RecvUnblocked(id) => {
+                        blocked_recv_streams.remove(&id);
+                    }
+                    StreamEvt::RecvClosed(id) => {
+                        // delete this stream from quinn and those lists
+                        // as I have nowhere to send the data
+                    }
+                    _ => todo!(),
                 }
             }
+
+            readable_streams.retain(|id| {
+                let ctx = recv_streams.get_mut(id).unwrap();
+                let mut stream = conn.recv_stream(*id);
+                loop {
+                    if ctx.closed.load(Ordering::Relaxed) {
+                        /* downstream closed, close this stream as I can do nothing more */
+                        /* wait for the RecvClosed signal, delete this stream there */
+                        break false;
+                    }
+
+                    /* if ctx.recv is full, I'm blocked, but this stream is still readable */
+                    /* downstream is too slow, wait for it consuming at least 25% of the buffer */
+                    if ctx.recv.is_full() {
+                        ctx.blocked.store(true, Ordering::Relaxed);
+                        blocked_recv_streams.insert(*id);
+                        break true;
+                    }
+
+                    let mut chunks = match stream.read(true) {
+                        Ok(chunks) => chunks,
+                        // closed, leave downstream to consume the remaining data
+                        // it will then close after checking the flag
+                        Err(ReadableError::ClosedStream) => {
+                            ctx.closed.store(true, Ordering::Release);
+                            break false
+                        },
+                        Err(ReadableError::IllegalOrderedRead) => {
+                            // something strange happened
+                            panic!("illegal ordered read")
+                        }
+                    };
+
+                    let slots = ctx.recv.slots();
+                    let mut write_chunk = ctx.recv.write_chunk_uninit(slots).unwrap();
+                    let slice = write_chunk.as_mut_slices().0;
+                    let mut written = 0;
+                    let mut readable = true;
+                    for slot in slice {
+                        match chunks.next(usize::MAX) {
+                            Ok(Some(chunk)) => {
+                                slot.write(chunk.bytes);
+                                written += 1;
+                            },
+                            Ok(None) => {
+                                ctx.closed.store(true, Ordering::Release);
+                                readable = false;
+                                break;
+                            }
+                            /* cannot produce more, remove this stream from the readable list and wait for Readable event */
+                            Err(ReadError::Blocked) => {
+                                readable = false;
+                                break;
+                            },
+                            Err(ReadError::Reset(err)) => {
+                                panic!("stream {} reset: {}", id, err)
+                            }
+                        }
+                    }
+                    unsafe { write_chunk.commit(written) };
+                    if !readable {
+                        break false;
+                    }
+                }
+            });
 
             writable_streams.retain(|id| {
                 let ctx = send_streams.get_mut(id).unwrap();
@@ -232,6 +319,7 @@ impl Runner {
                     /* if ctx.send is empty, I'm blocked, but this stream is still writable */
                     /* upstream is too slow, wait for it filling at least 25% of the buffer */
                     if ctx.send.is_empty() {
+                        // I'll be wakened up by the next poll_write or poll_flush
                         ctx.blocked.store(true, Ordering::Relaxed);
                         // the value of `closed` must be reliable, or I could sleep forever
                         // when the stream is closed, make sure that there's really nothing left
@@ -239,22 +327,20 @@ impl Runner {
                         which should be after a Release store of `closed` of upstream,
                         which is in turn after send.push of upstream */
                         if !ctx.closed.load(Ordering::Acquire) || ctx.send.is_empty() {
-                            blocked_streams.insert(*id);
+                            blocked_send_streams.insert(*id);
                             break true;
                         }
                     }
 
-                    let mut writable = true;
-
                     let slots = ctx.send.slots();
-                    let mut chunks = ctx.send.read_chunk(slots).unwrap();
-                    let slice = chunks.as_mut_slices().0;
+                    let mut read_chunk = ctx.send.read_chunk(slots).unwrap();
+                    let slice = read_chunk.as_mut_slices().0;
                     let expect = slice.len();
                     let written = stream.write_chunks(slice);
                     match written {
                         Ok(written) => {
                             let written = written.chunks;
-                            chunks.commit(written);
+                            read_chunk.commit(written);
                             /* did I just unblock upstream? test here */
                             if slots - written <= QUIC_STREAM_RECV_UNBLOCK_THRESHOLD {
                                 ctx.waker.wake();
