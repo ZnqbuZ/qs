fbe6549beeea148d809f65a8130e093dec42bb3b
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-01-16 12:02:51 +0100

    change some params

--- src/socket_new.rs
+++ src/socket_new.rs
@@ -273,14 +273,14 @@ mod tests {
         transport_config.initial_mtu(65535);
         transport_config.min_mtu(65535);
 
-        transport_config.stream_receive_window(VarInt::from_u32(64 * 1024 * 1024));
-        transport_config.receive_window(VarInt::from_u32(16 * 64 * 1024 * 1024));
-        transport_config.send_window(16 * 64 * 1024 * 1024);
+        transport_config.stream_receive_window(VarInt::from_u32(10 * 1024 * 1024));
+        transport_config.receive_window(VarInt::from_u32(15 * 1024 * 1024));
 
         transport_config.max_concurrent_bidi_streams(VarInt::from_u32(1024));
         transport_config.max_concurrent_uni_streams(VarInt::from_u32(1024));
 
         transport_config.datagram_receive_buffer_size(None);
+        transport_config.datagram_send_buffer_size(usize::MAX);
 
         transport_config.congestion_controller_factory(Arc::new(BbrConfig::default()));
 
